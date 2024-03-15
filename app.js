2bb8e0dd3de3bbc8340cf1480534da867bb6f23e
jxeffects <jxeffects@gmail.com>
2024-03-15 17:01:34 -0400

    Guess game files added

--- app.js
+++ app.js
@@ -4,3 +4,19 @@
 //         console.log(`   j is ${j}`);
 //     }
 // }
+let count = 0;
+let maxGuess = parseInt(prompt("WELCOME! Enter your max number"));
+// console.log(typeof (maxGuess))
+let random = Math.floor((Math.random() * maxGuess) + 1);
+console.log(random)
+let firstGuess = prompt("Enter your first guess!!");
+// console.log(typeof (firstGuess));
+while (maxGuess !== firstGuess) {
+    if (firstGuess > maxGuess) {
+        prompt("Too high");
+    }
+    else {
+        prompt("Too low");
+    }
+    console.log('You win ')
+}
