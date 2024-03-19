dc01666399bbd6f6c739ea55b07743855f0ed001
jxeffects <jxeffects@gmail.com>
2024-03-19 06:57:44 -0400

    add more features and css fixed

--- app.js
+++ app.js
@@ -6,14 +6,21 @@
 // }
 let count = 0;
 let max = parseInt(prompt("Welcome, Enter your max"));
+while (!max) {
+    max = parseInt(prompt("Please enter the valid integer!!!"));
+}
 let guess_max = Math.floor((Math.random() * max) + 1);
 let guess = parseInt(prompt("Enter your guess"));
-while (guess !== guess_max) {
-    if (guess > max)
-        guess = parseInt(prompt("Too high!!, Try again!!"));
-    else {
-        guess = parseInt(prompt("Too Low, Try again!!"));
+while (parseInt(guess) !== guess_max) {
+    if (guess > guess_max)
+        guess = prompt("Too high!!, Try again");
+    else if (guess < guess_max) {
+        guess = prompt("Too Low, Try again!!");
+    }
+    else if (guess === 'q') {
+        console.log("You quit"); break;
     }
     count += 1;
 }
-console.log(`You got right answer with ${count} times`)
+if (guess !== 'q')
+    console.log(`You got right answer with ${count} times`)
