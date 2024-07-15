07598b15543d2473f54c74abbc484c353916139b
Jatinkumar11098 <jxeffects@gmail.com>
2024-07-15 15:03:48 -0400

    Fixed NaN problem

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const player2 = document.querySelector('.button-2');
 const reset = document.querySelector('.button-3');
 const score1 = document.querySelector('#scrOne');
 const score2 = document.querySelector('#scrTwo');
+const range = document.querySelector('#rangeSelect');
 let counter1 = 0;
 let counter2 = 0;
 
+range.addEventListener('change', () => {
+    let value = parseInt(range.value);
+    console.log(typeof (value));
+    console.log(value);
 
+})
 player1.addEventListener('click', () => {
     counter1++;
     score1.textContent = counter1;
