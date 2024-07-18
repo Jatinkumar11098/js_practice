6edd081b69dffee11f19bb1c3f742c64a2ab09ae
Jatinkumar11098 <jxeffects@gmail.com>
2024-07-17 23:23:17 -0400

    refined functions

--- app.js
+++ app.js
@@ -6,46 +6,58 @@ const score2 = document.querySelector('#scrTwo');
 const range = document.querySelector('#rangeSelect');
 let counter1 = 0;
 let counter2 = 0;
+let gameOver = false;
+let value = 0;
 
-range.addEventListener('change', () => {
-    let value = parseInt(range.value);
-    let gameOver = false;
 
-    player1.addEventListener('click', () => {
-        if (!gameOver) {
-            if (counter1 < value) {
-                counter1++;
-            }
-            else {
-                gameOver = true;
-                console.log('gameover')
+player1.addEventListener('click', () => {
+    if (!gameOver) {
+        if (counter1 !== value) {
+            counter1++;
+        }
+        else {
+            gameOver = true;
+            if (value !== 0 && counter1 !== 0) {
+                score1.classList.add('text-success');
+                score2.classList.add('text-danger');
             }
         }
-        score1.textContent = counter1;
-    })
-
-    player2.addEventListener('click', () => {
-        if (!gameOver) {
-            if (counter2 < value) {
-                counter2++;
-            } else {
-                gameOver = true;
-            }
-        }
-        score2.textContent = counter2;
-    })
+    }
+    score1.textContent = counter1;
 })
 
+player2.addEventListener('click', () => {
+    if (!gameOver) {
+        if (counter2 !== value) {
+            counter2++;
+        } else {
+            gameOver = true;
+            if (value !== 0 && counter2 !== 0) {
+                score2.classList.add('text-success');
+                score1.classList.add('text-danger');
+            }
 
+        }
+    }
+    score2.textContent = counter2;
+})
 
+range.addEventListener('change', () => {
+    value = parseInt(range.value);
+    resetFunc();
 
+});
 
-reset.addEventListener('click', () => {
+reset.addEventListener('click', resetFunc)
+
+function resetFunc() {
     counter1 = 0;
     counter2 = 0;
     score1.textContent = 0;
     score2.textContent = 0;
     gameOver = false;
-
-
-})
+    score2.classList.remove('text-success');
+    score1.classList.remove('text-danger');
+    score1.classList.remove('text-success');
+    score2.classList.remove('text-danger');
+}
